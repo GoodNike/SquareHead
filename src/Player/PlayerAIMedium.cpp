39a0e2a255e2f003b04cc3c96f1b31c5fec1b664
Krasheninnikov, Nikolai <nkrasheninnikov@yandex.ru>
2018-12-12 21:38:11 +0300

    Переработан ИИ среднего уровня: теперь его ход занимает фиксированное количество манипуляций с игровым полем. Стал значительно быстрее. Основной принцип хода тот же, так что ходит так же.

--- src/Player/PlayerAIMedium.cpp
+++ src/Player/PlayerAIMedium.cpp
@@ -11,8 +11,8 @@ PlayerAIMedium::~PlayerAIMedium() = default;
 Qt::GlobalColor PlayerAIMedium::makeTurn()
 {
     // Алгоритм: из всех цветов, которыми можно ходить, выберем тот, который даст наибольшую разницу
-    // между прибавкой очков АИ и соперника при условии, что соперник делает наиболее оптимальный с
-    // точки зрения своих очков ход.
+    // между прибавкой очков АИ и очков соперника при условии, что соперник делает наиболее оптимальный
+    // с точки зрения своих очков ход.
 
     // Выберем из игроков первого соперника.
     auto players = field().players();
@@ -24,27 +24,50 @@ Qt::GlobalColor PlayerAIMedium::makeTurn()
     }
     const auto vsPlayer = *vsPlayerRef;
 
+    // Сделаем все возможные хода каждого игрока и подсчитаем очки.
+    QVector<QPair<Qt::GlobalColor, int>> vsPlayerScore;
+    QVector<QPair<Qt::GlobalColor, int>> aiPlayerScore;
+    for (const auto &i : field().colors()) {
+        Field f = field();
+        if (i != vsPlayer->color()) {
+            f.playersTurn(vsPlayer->pos(), i);
+            vsPlayerScore.push_back( qMakePair(i, f.calcScore(vsPlayer->pos())) );
+        }
+        // Первый игрок (т.е. ИИ) не может пойти своим цветом и цветом соперника.
+        if (i != color() && i != vsPlayer->color()) {
+            f.playersTurn(pos(), i);
+            aiPlayerScore.push_back( qMakePair(i, f.calcScore(pos())) );
+        }
+    }
+
+    // Отсортируем - в начале самые успешные ходы.
+    auto lessFunc = [] (auto &&a, auto &&b) {
+        return std::tie(a.second, a.first) < std::tie(b.second, b.first);
+    };
+    std::sort(std::begin(vsPlayerScore), std::end(vsPlayerScore), lessFunc);
+    std::sort(std::begin(aiPlayerScore), std::end(aiPlayerScore), lessFunc);
+
+    // Если у соперника нет ходов, то что-то пошло не так: такого быть не должно.
+    if (vsPlayerScore.size() < 2) {
+        return Qt::transparent;
+    }
+
+    // У соперника нет вариантов - самый успешный ход, и ход если этот цвет выберут.
+    auto vsPlayerTurn = vsPlayerScore[0];
+    auto vsPlayerAlterTurn = vsPlayerScore[1];
+
     Qt::GlobalColor preferredColor = Qt::transparent;
     int ds = std::numeric_limits<int>::min();
-    int aiScore_n1 = field().calcScore( pos() );
-
-    for (const auto i : field().availableColors()) {
-        Field f = field();
-        f.playersTurn( pos(), i );
-        int aiScore = f.calcScore( pos() );
-
-        int vsScore = std::numeric_limits<int>::min();
-        for (const auto j : f.availableColors()) {
-            Field vsField = f;
-            vsField.playersTurn(vsPlayer->pos(), j);
-            vsScore = std::max( vsScore, vsField.calcScore( vsPlayer->pos() ) );
-        }
+    int aiScore_n1 = field().calcScore(pos());
+    for (const auto &i : aiPlayerScore) {
+        int aiScore = i.second;
+        int vsScore = (i.first != vsPlayerTurn.first) ? vsPlayerTurn.second : vsPlayerAlterTurn.second;
 
         // Выберем ход если он дает не только наилучший результат по разницу очков, но и какой-то прирост
         // для себя. Иначе возможен выбор хода без прироста для себя, но с максимальным вредом для соперника.
         if (aiScore - aiScore_n1 > 0 && aiScore - vsScore > ds) {
             ds = aiScore - vsScore;
-            preferredColor = i;
+            preferredColor = i.first;
         }
     }
 
