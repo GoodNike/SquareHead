6fccc5e04f7626bd00e35ec0747cf6b9fee0fa68
Krasheninnikov, Nikolai <nkrasheninnikov@yandex.ru>
2018-12-14 19:28:16 +0300

    Добавлена версионность при загрузке настроек.

--- src/GlobalOptions.cpp
+++ src/GlobalOptions.cpp
@@ -19,29 +19,52 @@ struct options_t {
         loadSettings();
     }
 
-    void loadSettings()
+    void loadDefaults()
     {
-        QSettings settings(orgName, appName);
-
         PlayerSettings p1;
-        Settings::playerType(p1) = settings.value("player1/type", QVariant::fromValue(Player_t::Human).toInt()).value<Player_t>();
-        Settings::playerPos(p1) = settings.value("player1/pos", QVariant::fromValue(PlayersPos_t::BottomRight)).value<PlayersPos_t>();
-        Settings::playerName(p1) = settings.value("player1/name", "Player").toString();
+        Settings::playerType(p1) = Player_t::Human;
+        Settings::playerPos(p1) = PlayersPos_t::BottomRight;
+        Settings::playerName(p1) = "Player";
 
         PlayerSettings p2;
-        Settings::playerType(p2) = settings.value("player2/type", QVariant::fromValue(Player_t::AI_Hard).toInt()).value<Player_t>();
-        Settings::playerPos(p2) = settings.value("player2/pos", QVariant::fromValue(PlayersPos_t::TopLeft)).value<PlayersPos_t>();
-        Settings::playerName(p2) = settings.value("player2/name", "AI_Hard").toString();
+        Settings::playerType(p2) = Player_t::AI_Hard;
+        Settings::playerPos(p2) = PlayersPos_t::TopLeft;
+        Settings::playerName(p2) = "AI_Hard";
 
         players.push_back(p1);
         players.push_back(p2);
     }
 
+    void loadSettings()
+    {
+        QSettings settings(orgName, appName);
+
+        const QString version = settings.value("version").toString();
+        if (version != VERSION_STRING) {
+            loadDefaults();
+            saveSettings();
+        } else {
+            PlayerSettings p1;
+            Settings::playerType(p1) = settings.value("player1/type", QVariant::fromValue(Player_t::Human).toInt()).value<Player_t>();
+            Settings::playerPos(p1) = settings.value("player1/pos", QVariant::fromValue(PlayersPos_t::BottomRight)).value<PlayersPos_t>();
+            Settings::playerName(p1) = settings.value("player1/name", "Player").toString();
+
+            PlayerSettings p2;
+            Settings::playerType(p2) = settings.value("player2/type", QVariant::fromValue(Player_t::AI_Hard).toInt()).value<Player_t>();
+            Settings::playerPos(p2) = settings.value("player2/pos", QVariant::fromValue(PlayersPos_t::TopLeft)).value<PlayersPos_t>();
+            Settings::playerName(p2) = settings.value("player2/name", "AI_Hard").toString();
+
+            players.push_back(p1);
+            players.push_back(p2);
+        }
+    }
+
     void saveSettings()
     {
-        if (players.size() >= 2) {
-            QSettings settings(orgName, appName);
+        QSettings settings(orgName, appName);
 
+        settings.setValue("version", VERSION_STRING);
+        if (players.size() >= 2) {
             settings.setValue( "player1/type", static_cast<int>( Settings::playerType(players[0]) ) );
             settings.setValue( "player1/pos", static_cast<int>( Settings::playerPos(players[0]) ) );
             settings.setValue( "player1/name", Settings::playerName(players[0]) );
