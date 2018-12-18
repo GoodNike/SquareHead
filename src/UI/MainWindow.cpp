cdea4f463de5751669a97f044b1a5745d52da9ad
GoodNike <nkrasheninnikov@yandex.ru>
2018-12-18 11:17:05 +0300

    Выполнен переход на C++14 с C++17 для того, чтобы не было проблем при сборке snap.

--- src/UI/MainWindow.cpp
+++ src/UI/MainWindow.cpp
@@ -139,7 +139,9 @@ MainWindow::MainWindow(QWidget *parent)
         m_game.field().removeAllPlayers();
 
         for (const auto &i : GlobalOptions::instance().players()) {
-            const auto &[player, pos, name] = i;
+            const auto &player = Settings::playerType(i);
+            const auto &pos = Settings::playerPos(i);
+            const auto &name = Settings::playerName(i);
             if (player == Player_t::Human) {
                 m_turnAcceptor = m_game.field().addPlayer(pos, name, player);
             } else {
